3dec51134306d176b4b27f4ed45cf0897262bf76
Paajicek <cihin44@gmail.com>
2025-05-17 23:46:45 +0200

    Zpět přímé připojení k DB db4free.net

--- main.py
+++ main.py
@@ -1,92 +1,75 @@
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import mysql.connector
-from datetime import datetime
 import gspread
-import os
-import json
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Připojení k databázi
-db = mysql.connector.connect(
-    host="sql8.freemysqlhosting.net",
-    user="sql8683763",
-    password="hVXbkJ6tbB",
-    database="sql8683763"
-)
+# Přímé připojení k MySQL databázi
+try:
+    db = mysql.connector.connect(
+        host="db4free.net",
+        user="paajicek",
+        password="Bohemka1905",
+        database="esatenis"
+    )
+except mysql.connector.Error as err:
+    print("Chyba při připojení k databázi:", err)
+    raise
 
-# Parametry pro výpočet
-p_tour_ace = 8.9  # tvrdý povrch, muži
-p_tour_df = 3.5
+cursor = db.cursor()
 
-# Výpočet es a dvojchyb
-def vypocet(gender, ah, df, vah, p_tour_ace, p_tour_df, vs_vace, vs_gender):
-    s = 70 if gender == "M" else 65
-    ph = ah
-    ps = vs_vace
-    esa = s * ph / 100 * (ps / p_tour_ace)
-    dvojchyby = s * df / 100 * (p_tour_df / 3.5)
-    return round(esa, 2), round(dvojchyby, 2)
+# Připojení ke Google Sheets
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("google_key.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("Esa zápasy").sheet1
+
+# Průměrné hodnoty pro výpočty
+p_tour_ace = 4.2
+p_tour_df = 6.0
 
 @app.get("/", response_class=HTMLResponse)
 async def form(request: Request):
-    cursor = db.cursor()
-    cursor.execute("SELECT Player FROM esa_prepared ORDER BY Player")
+    cursor.execute("SELECT DISTINCT Player FROM esa_prepared ORDER BY Player")
     players = [row[0] for row in cursor.fetchall()]
     return templates.TemplateResponse("form.html", {"request": request, "players": players})
 
 @app.get("/result", response_class=HTMLResponse)
 async def result(request: Request, player1: str, player2: str):
-    cursor = db.cursor()
     cursor.execute("SELECT * FROM esa_prepared WHERE Player = %s", (player1,))
     row1 = cursor.fetchone()
-    cursor.fetchall()  # Vyčistí předchozí výsledky
     cursor.execute("SELECT * FROM esa_prepared WHERE Player = %s", (player2,))
     row2 = cursor.fetchone()
-    cursor.fetchall()
 
     if not row1 or not row2:
-        return templates.TemplateResponse("error.html", {"request": request, "msg": "Hráč nenalezen"})
+        return templates.TemplateResponse("form.html", {
+            "request": request,
+            "players": [player1, player2],
+            "error": "Nebyli nalezeni oba hráči v databázi."
+        })
 
-    esa1, df1 = vypocet(*row1[1:], p_tour_ace, p_tour_df, row2[3], row1[1])
-    esa2, df2 = vypocet(*row2[1:], p_tour_ace, p_tour_df, row1[3], row2[1])
-    celkem_esa = round(esa1 + esa2, 2)
-    celkem_df = round(df1 + df2, 2)
+    def vypocet(player, gender, ah, df, vah, p_tour_ace, p_tour_df, vace, vdf):
+        S = 65 if gender == "W" else 70
+        E = S * ah * (vace / p_tour_ace)
+        D = S * df * (vdf / p_tour_df)
+        return round(E, 2), round(D, 2)
 
-    # Zápis do Google Sheets
-    try:
-        service_account_info = json.loads(os.getenv("GOOGLE_KEY_JSON"))
-        gc = gspread.service_account_from_dict(service_account_info)
-        sh = gc.open("Aceapp")
-        worksheet = sh.worksheet("Esa výsledky")
+    esa1, df1 = vypocet(*row1, p_tour_ace, p_tour_df, row2[4], row2[3])
+    esa2, df2 = vypocet(*row2, p_tour_ace, p_tour_df, row1[4], row1[3])
 
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        data_row = [
-            now, player1, row1[2], row1[3], row1[4], esa1, df1,
-            player2, row2[2], row2[3], row2[4], esa2, df2,
-            celkem_esa, celkem_df
-        ]
-        worksheet.append_row(data_row)
-    except Exception as e:
-        print("Chyba při zápisu do Google Sheets:", e)
+    sheet.append_row([player1, esa1, df1, player2, esa2, df2])
 
-    return templates.TemplateResponse("result.html", {
+    return templates.TemplateResponse("form.html", {
         "request": request,
+        "players": [player1, player2],
         "player1": player1,
-        "ace1": row1[2],
-        "vace1": row1[3],
-        "df1": row1[4],
-        "esa1": esa1,
-        "dvojchyby1": df1,
         "player2": player2,
-        "ace2": row2[2],
-        "vace2": row2[3],
-        "df2": row2[4],
+        "esa1": esa1,
+        "df1": df1,
         "esa2": esa2,
-        "dvojchyby2": df2,
-        "celkem_esa": celkem_esa,
-        "celkem_df": celkem_df
+        "df2": df2
     })
